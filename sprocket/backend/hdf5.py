0499b0b5a491b499d348edc13c23302ddcc01afa
Kazuhiro Kobayashi <root.4mac@gmail.com>
2017-06-07 22:35:30 +0900

    ADD: modify directory setting and add some files

---
+++ sprocket/backend/hdf5.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin/python
+# -*- coding: utf-8 -*-
+#
+# hdf5.py
+#   First ver.: 2017-06-07
+#
+#   Copyright 2017
+#       Kazuhiro KOBAYASHI <kobayashi.kazuhiro@g.sp.m.is.nagoya-u.ac.jp>
+#
+#   Distributed under terms of the MIT license.
+#
+
+"""
+
+
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
