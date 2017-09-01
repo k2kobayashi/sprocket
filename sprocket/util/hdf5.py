293a9d2001928a88fdde41dbba45f0aebe08aecc
k2kobayashi <root.4mac@gmail.com>
2017-09-01 17:51:42 +0900

    remove HDFFiles and add read_feats to read features from h5 based on the given list

--- sprocket/util/hdf5.py
+++ sprocket/util/hdf5.py
@@ -98,52 +98,37 @@ class HDF5(object):
         return
 
 
-class HDF5files(object):
+def read_feats(listf, h5dir, ext='mcep'):
+    """HDF5 handler
+    Create list consisting of arrays listed in the list
 
-    """HDF5 files class
-    This class offers to open several hdf5 files using list
+    Parameters
+    ---------
+    listf : str,
+        Path of list file
+
+    h5dir : str,
+        Path of hdf5 directory
+
+    ext : str,
+        `mcep` : mel-cepstrum
+        `f0` : F0
+
+    Returns
+    ---------
+    datalist : list of arrays
 
     """
 
-    def __init__(self, listf, h5dir):
-        # open list file
-        with open(listf, 'r') as fp:
-            files = fp.readlines()
+    datalist = []
+    with open(listf, 'r') as fp:
+        files = fp.readlines()
 
-        self.num_files = len(files)
-        self.h5list = []
-        for f in files:
-            # open list file
-            f = f.rstrip()
-            h5f = h5dir + '/' + f + '.h5'
-            self.h5list.append(HDF5(h5f, mode='r'))
+    for f in files:
+        f = f.rstrip()
+        h5f = h5dir + '/' + f + '.h5'
+        h5 = HDF5(h5f, mode='r')
+        datalist.append(h5.read(ext))
+        h5.close()
 
-        return
-
-    def datalist(self, ext=None):
-        """Open several hdf5 files based on the extention
-
-        Parameters
-        ---------
-        ext : str,
-            File extention of label in hdf5 file
-
-        Returns
-        ---------
-        datalist : list, shape (`self.num_files`)
-            List of several data
-
-        """
-
-        datalist = []
-        for i in range(self.num_files):
-            datalist.append(self.h5list[i].read(ext))
-
-        return datalist
-
-    def close(self):
-        # close hdf5 files
-        for i in range(self.num_files):
-            self.h5list[i].close()
-
-        return
+    return datalist
