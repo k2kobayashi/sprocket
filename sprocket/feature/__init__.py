d7c5efa4325222999f6c2f58941d7baab5ff63c1
k2kobayashi <root.4mac@gmail.com>
2017-09-01 17:50:01 +0900

    add non-negative check for f0 extraction

--- sprocket/feature/__init__.py
+++ sprocket/feature/__init__.py
@@ -92,6 +92,9 @@ class FeatureExtractor(object):
         """
         self._f0, self._spc, self._ap = self.analyzer.analyze(self.x)
 
+        # check non-negative for F0
+        self._f0[0 < self._f0] = 0
+
         return
 
     def f0(self):
