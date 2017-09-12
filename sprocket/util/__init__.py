25bf06f2094f3c76d79a4ee70e418eed328a349e
Kazuhiro Kobayashi <root.4mac@gmail.com>
2017-09-12 12:06:51 +0900

    add interface of util to __init__.py

--- sprocket/util/__init__.py
+++ sprocket/util/__init__.py
@@ -1,1 +1,5 @@
-from .delta import delta
+from .delta import delta, construct_static_and_delta_matrix
+from .distance import melcd
+from .extfrm import extfrm
+from .hdf5 import HDF5, read_feats
+from .jnt import JointFeatureExtractor
