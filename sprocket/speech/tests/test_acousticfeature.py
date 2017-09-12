e7a4ed117f318e669aafc3556a6c839812c7efea
Kazuhiro Kobayashi <root.4mac@gmail.com>
2017-09-12 09:06:51 +0900

    modify test code to adapt new interfaces

---
+++ sprocket/speech/tests/test_acousticfeature.py
@@ -0,0 +1,76 @@
+from __future__ import division, print_function, absolute_import
+
+import unittest
+import os
+
+import numpy as np
+from scipy.io import wavfile
+import pyworld
+
+from sprocket.speech import FeatureExtractor, Synthesizer
+
+dirpath = os.path.dirname(os.path.realpath(__file__))
+
+
+class AnalysisSynthesisTest(unittest.TestCase):
+
+    def test_anasyn_16000(self):
+        path = dirpath + '/data/test16000.wav'
+        fs, x = wavfile.read(path)
+        af = FeatureExtractor(analyzer='world', fs=fs, shiftms=5)
+        f0, _, ap = af.analyze(x)
+        mcep = af.mcep(dim=24, alpha=0.42)
+        synth = Synthesizer()
+
+        assert len(np.nonzero(f0)[0]) > 0
+
+        # synthesize F0, mcep, ap
+        wav = synth.synthesis(f0, mcep, ap, alpha=0.42,
+                              fftl=1024, fs=fs, shiftms=5)
+        nun_check(wav)
+
+    def test_anasyn_44100(self):
+        path = dirpath + '/data/test44100.wav'
+        fs, x = wavfile.read(path)
+        af = FeatureExtractor(analyzer='world', fs=fs, shiftms=5)
+        f0, _, ap = af.analyze(x)
+        mcep = af.mcep(dim=40, alpha=0.50)
+        synth = Synthesizer()
+
+        assert len(np.nonzero(f0)[0]) > 0
+
+        # mcep synthesis
+        wav = synth.synthesis(f0, mcep, ap, alpha=0.50,
+                              fftl=2048, fs=fs, shiftms=5)
+        nun_check(wav)
+
+    def test_spc_and_npow(self):
+        path = dirpath + '/data/test16000.wav'
+        fs, x = wavfile.read(path)
+        af = FeatureExtractor(analyzer='world', fs=fs, shiftms=5)
+        _, spc, _ = af.analyze(x)
+        npow = af.npow()
+        assert spc.shape[0] == npow.shape[0]
+
+    def test_synthesis_from_bandap(self):
+        path = dirpath + '/data/test16000.wav'
+        fs, x = wavfile.read(path)
+        minf0 = 60
+        af = FeatureExtractor(analyzer='world', fs=fs,
+                              shiftms=5, minf0=minf0)
+        f0, spc, ap = af.analyze(x)
+        bandap = af.bandap()
+
+        assert pyworld.get_num_aperiodicities(fs) == bandap.shape[-1]
+
+        fftsize = pyworld.get_cheaptrick_fft_size(fs, minf0)
+        ap = pyworld.decode_aperiodicity(bandap, fs, fftsize)
+
+        synth = Synthesizer()
+        wav = synth.synthesis_spc(f0, spc, ap, fs, shiftms=5)
+        nun_check(wav)
+
+
+def nun_check(wav):
+    if any(np.isnan(wav)):
+        raise ('wavform consists NaN value.')
