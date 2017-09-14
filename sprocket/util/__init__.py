95399df50bd85ccdd5c4e5d58631ed6ce33387f0
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2017-09-14 12:03:35 +0900

    change import path

--- sprocket/util/__init__.py
+++ sprocket/util/__init__.py
@@ -1,5 +1,5 @@
-from .delta import delta, construct_static_and_delta_matrix
+from .delta import delta, sddata, construct_static_and_delta_matrix
 from .distance import melcd
 from .extfrm import extfrm
 from .hdf5 import HDF5, read_feats
-from .jnt import JointFeatureExtractor
+from .jnt import estimate_twf
