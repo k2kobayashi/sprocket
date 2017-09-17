06547e34cceedbb3cd354538df0d1b8db3530287
Kazuhiro Kobayashi <root.4mac@gmail.com>
2017-09-17 23:54:03 +0900

    modify __init__.py for static_delta

--- sprocket/util/__init__.py
+++ sprocket/util/__init__.py
@@ -1,4 +1,4 @@
-from .delta import delta, sddata, construct_static_and_delta_matrix
+from .delta import delta, static_delta, construct_static_and_delta_matrix
 from .distance import melcd
 from .extfrm import extfrm
 from .hdf5 import HDF5
