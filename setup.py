1d3a06a1b087f3e30b8dd3956c561f71e342051d
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2018-12-05 14:43:46 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -10,23 +10,29 @@ else:
 CHANGES = open('CHANGES.md').read()
 
 setup(
+    # package info
     name="sprocket-vc",
     version="0.18",
     description="Voice conversion software",
     url='https://github.com/k2kobayashi/sprocket',
-    author='Kazuhiro Kobayashi',
-    packages=find_packages(exclude=('tests')),
-    long_description=(README + '\n' + CHANGES),
     license='MIT',
+
+    # author details
+    author='Kazuhiro Kobayashi',
+    author_email='root.4mac@gmail.com',
+
+    # package
+    packages=find_packages(exclude=('docs', 'tests')),
+    long_description=(README + '\n' + CHANGES),
+
+    # requirements
     install_requires=open('requirements.txt').readlines(),
-    extra_require={
-        'develop': [
-            'nose',
-            'docopt',
-        ],
+    extras_require={
+        'develop': ['nose','docopt'],
     },
-    classifier=[
-    'License :: OSI Approved :: MIT License',
-    'Topic :: Multimedia :: Sound/Audio :: Speech',
-    ]
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+    ],
 )
