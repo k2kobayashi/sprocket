2eb5c0e6e14f8f23cbc0c22fbb7219bace77941b
Kazuhiro Kobayashi <root.4mac@gmail.com>
2018-12-10 11:14:02 +0900

    add assertion to check data length for filtering

--- sprocket/util/filter.py
+++ sprocket/util/filter.py
@@ -22,6 +22,10 @@ def low_pass_filter(data, cutoff, fs, n_taps=255):
     modified data: array, shape (`T`, `dim`)
         Array of modified sequence.
     """
+    if data.shape[0] < n_taps * 3:
+        raise ValueError(
+            'Length of data should be three times longer than n_taps.')
+
     fil = firwin(n_taps, cutoff, pass_zero=True, nyq=fs//2)
     modified_data = filtfilt(fil, 1, data, axis=0)
     return modified_data
@@ -46,6 +50,10 @@ def high_pass_filter(data, cutoff, fs, n_taps=255):
     modified data: array, shape (`T`, `dim`)
         Array of modified sequence.
     """
+    if data.shape[0] < n_taps * 3:
+        raise ValueError(
+            'Length of data should be three times longer than n_taps.')
+
     fil = firwin(n_taps, cutoff, pass_zero=False, nyq=fs//2)
     modified_data = filtfilt(fil, 1, data, axis=0)
     return modified_data
